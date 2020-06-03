489104eee4798279bc4e93e91b8e5c6b5672fb6f
ivanm <IT0421ta24SA>
2020-06-03 16:03:19 -0500

    Se hace una prueba hacia master

--- SpringMvcCRUDExample/src/main/java/com/jackrutorial/controller/AutosController.java
+++ SpringMvcCRUDExample/src/main/java/com/jackrutorial/controller/AutosController.java
@@ -14,6 +14,8 @@ import com.jackrutorial.model.Autos;
 import com.jackrutorial.service.AutosService;
 
 
+//Se agrga comentario
+
 @Controller
 @RequestMapping(value = "/auto")
 public class AutosController {
